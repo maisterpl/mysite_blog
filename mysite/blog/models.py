7f8c4bed1431965c4e479013cc7c1d925fbc74b3
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-03-23 22:25:31 +0100

    first commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
+
+
+# Create own manager
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishManager,
+                     self).get_queryset()\
+                     .filter(status='published')
+                     
+
+# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICE = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        )
+    
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICE,
+                              default='draft')
+    
+    class Meta:
+        ordering = ('-publish',)
+        
+    def __str__(self) -> str:
+        return self.title
+    
+    objects = models.Manager()      # default Manager
+    published = PublishManager()    # non deafault Manager
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
+        
+    tags = TaggableManager()
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ('created',)
+        
+    def __str__(self) -> str:
+        return f'Komentarz dodany przez { self.name } dla posta { self.post }'
+    
+    
